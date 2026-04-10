37147a482eb437ad2d1a564ec235d5be1e63a700
Putz Paul <P.Putz@htlvb.at>
2026-04-10 08:03:25 +0200

    First commmit

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _20260410_IP_Adressen_Scanner
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
