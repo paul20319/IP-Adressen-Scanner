c5efacfe8bc7e01e18ede04d7e9880ee81a21fdd
S.Schoegl <S.Schoegl@E1C03>
2026-04-10 08:19:06 +0200

    Basisinformationen

--- Program.cs
+++ Program.cs
@@ -1,10 +1,22 @@
-﻿namespace _20260410_IP_Adressen_Scanner
+﻿
+
+
+namespace _20260410_IP_Adressen_Scanner
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static async void Main(string[] args)
         {
             Console.WriteLine("IP-Scanner von Paul und Sebastian");
+
+            string baseIp = "192.168.1."; 
+
+            ´var tasks = new List<Task>();
+
+            
+             
+
+
         }
     }
 }
